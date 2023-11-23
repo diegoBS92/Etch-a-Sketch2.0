5b44c5e69232bcda53f6d61304951b87958f8c17
DiegoGo <alberto.android.92@gmail.com>
2023-11-22 22:01:30 -0600

    added the feature where you can only add a number from 2 to 100, random colors, and styled a little bit

--- script.js
+++ script.js
@@ -1,18 +1,23 @@
 
-let container = document.querySelector('.container')
+let container = document.querySelector('.board')
 let btn = document.querySelector('.btn')
 let number = document.querySelector('#number')
 
 let generateBoard = () => {
-     let boardNum = number.value
+     let boardNum = +(number.value)
      container.innerHTML = ''
 
-     for (let squares = 0; squares < boardNum*boardNum ; squares++) {
-        let div = document.createElement('div')
-        div.classList.add('child')
-        div.addEventListener('mouseover', ()=> div.classList.add('painted'))
-        div.setAttribute('style', `width: ${500/boardNum}px; heigth: ${500/boardNum}; box-sizing: border-box; border: 1px red solid;`)
-        container.appendChild(div)
+     if(boardNum > 100 || boardNum < 2) {
+      alert('please introduce a number between 2 and 100')
+      
+     } else {
+      for (let squares = 0; squares < boardNum*boardNum ; squares++) {
+         let div = document.createElement('div')
+         div.classList.add('child')
+         div.addEventListener('mouseover', ()=> div.style.background = `rgb(${Math.trunc(Math.random()*255)}, ${Math.trunc(Math.random()*255)}, ${Math.trunc(Math.random()*255)})`)
+         div.setAttribute('style', `width: ${500/boardNum}px; heigth: ${500/boardNum}; box-sizing: border-box; `)
+         container.appendChild(div)
+      }
      }
 }
 btn.addEventListener('click', generateBoard)
